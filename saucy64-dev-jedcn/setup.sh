e05a64a5a8da7f2a8389dd29566abddc9e341442
Jed Northridge <northridge@gmail.com>
2013-12-15 15:56:30 -0500

    [jedcn-dev setup] Support "homesick is already installed" scenario

--- saucy64-dev-jedcn/setup.sh
+++ saucy64-dev-jedcn/setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash -ex
 
-echo "Installing homeshick.."
-git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+# This is intended to be run as follows:
+#
+# curl https://raw.github.com/jedcn/dev-machines/master/saucy64-dev-jedcn/setup.sh | /bin/bash
 
-echo "Adding Castles.."
-homeshick clone --batch https://github.com/robbyrussell/oh-my-zsh
-homeshick clone https://github.com/jedcn/dot-org-files
-cd; ln -s .homesick/repos/oh-my-zsh .oh-my-zsh
+if [ -d $HOME/.homesick/repos/homeshick ]
+then
+  echo ""
+  echo "Homesick already installed.."
+else
+  echo ""
+  echo "Installing homeshick.."
+  git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+  source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+
+  echo ""
+  echo "Adding Castles.."
+  homeshick clone --batch https://github.com/robbyrussell/oh-my-zsh
+  homeshick clone --batch https://github.com/jedcn/dot-org-files
+  homeshick link --force
+
+  cd; ln -s .homesick/repos/oh-my-zsh .oh-my-zsh
+fi
